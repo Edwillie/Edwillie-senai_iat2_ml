8fb97d269beff9304a2a31101abbe876f5914709
Edwillie <edwillie@gmail.com>
2026-01-28 20:35:46 -0300

    intervalo

---
+++ Aula 01/exemplo01.py
@@ -0,0 +1,7 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix, classification_report
+
+dados = load_breast_cancer()
