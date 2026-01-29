78d0074c89c75000121224aefa742e9494609cc1
Edwillie <edwillie@gmail.com>
2026-01-28 21:04:21 -0300

    depois intervalo

--- Aula 01/exemplo01.py
+++ Aula 01/exemplo01.py
@@ -4,4 +4,15 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import confusion_matrix, classification_report
 
+#Carregando o modelo padronizado e validado
 dados = load_breast_cancer()
+X = dados.data   # Os atributos - features
+Y = dados.target # Os rotulos/alvos - target
+
+#Pre-processamento dos dados do dataset
+escala = StandardScaler()
+X_escalonado = escala.fit_transform(X)
+
+
+
+
