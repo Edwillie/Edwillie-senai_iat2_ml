312adfba000d7c6e3c9727da64dbc080a9569488
Edwillie <edwillie@gmail.com>
2026-02-11 22:00:25 -0300

    .

--- Aula 03/bayesiana.py
+++ Aula 03/bayesiana.py
@@ -7,9 +7,9 @@ def criarSistema():
     modelo = DiscreteBayesianNetwork([('Painel','Vazao'), ('Filtro', 'Vazao'), ('Tanque', 'Vazao'), ('Motor', 'Vazao'), ('Fluido', 'Vazao'), ('Tanque', 'Sensor')])
 
     # Criar estrutura de interação direta com o problema
-    cpdPainel = TabularCPD(variable='Painel', variable_card=2, values=[0, 1])
-    cpdFiltro = TabularCPD(variable='Filtro', variable_card=2, values=[0, 1])
-    cpdTanque = TabularCPD(variable='Tanque', variable_card=2, values=[0, 1])
+    cpdPainel = TabularCPD(variable='Painel', variable_card=2, values=[[0.01],[0.99]])
+    cpdFiltro = TabularCPD(variable='Filtro', variable_card=2, values=[[0.1],[0.9]])
+    cpdTanque = TabularCPD(variable='Tanque', variable_card=2, values=[[0.05],[0.95]])
 
 def sistema():
     pass
