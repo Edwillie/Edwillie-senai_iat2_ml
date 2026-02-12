9fc88cfb54921b8138189573d892244856ef0c27
Edwillie <edwillie@gmail.com>
2026-02-11 22:01:10 -0300

    .

--- Aula 03/bayesiana.py
+++ Aula 03/bayesiana.py
@@ -11,5 +11,7 @@ def criarSistema():
     cpdFiltro = TabularCPD(variable='Filtro', variable_card=2, values=[[0.1],[0.9]])
     cpdTanque = TabularCPD(variable='Tanque', variable_card=2, values=[[0.05],[0.95]])
 
+    #proxima aula - cpdSensor=
+
 def sistema():
     pass
