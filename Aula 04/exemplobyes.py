e982ed799303cc3643f9c30254a5ad368c5f9689
Edwillie <edwillie@gmail.com>
2026-03-04 21:33:26 -0300

    Fechando modelo baiesiano

--- Aula 04/exemplobyes.py
+++ Aula 04/exemplobyes.py
@@ -5,9 +5,9 @@ from pgmpy.factors.discrete import TabularCPD
 from pgmpy.inference import VariableElimination
 
 COMPONENTES = {
-    'Painel': {0: 0.05, 1:0.99},
-    'Filtro': {0: 0.00, 1:1.00},
-    'Tanque': {0: 0.00, 1:1.00}
+    'Painel': {0: 0.05, 1: 0.99},
+    'Filtro': {0: 0.00, 1: 1.00},
+    'Tanque': {0: 0.00, 1: 1.00}
 }
 
 def calcularProbVazao(pPainel, pFiltro, pTanque):
@@ -29,7 +29,7 @@ def gerarResposta():
         probSim.append(probTerVazao)
         probNao.append(1 - probTerVazao)
 
-    return TabularCPD(variable = 'Vazao', variable_card = 2, values = [probNao, probSim], evidence=['Painel', 'Filtro', 'Tanque'], evidence_card = {2, 2, 2} )
+    return TabularCPD(variable = 'Vazao', variable_card=2, values=[probNao, probSim], evidence=['Painel', 'Filtro', 'Tanque'], evidence_card=[2, 2, 2] )
 
 def criarSistema():
     # Configuração do modelo
@@ -44,13 +44,13 @@ def criarSistema():
     # Criar estruturas de interação 
     # direta com o problema
     cpdPainel = TabularCPD(variable='Painel', variable_card=2, values= [[0.01],[0.99]] )
-    cpdFiltro = TabularCPD(variable='Filtro',variable_card=2, values=[[0.1],[0.9]])
-    cpdTanque = TabularCPD(variable='Tanque',variable_card=2, values=[[0.05],[0.95]])
+    cpdFiltro = TabularCPD(variable='Filtro', variable_card=2, values=[[0.1],[0.9]])
+    cpdTanque = TabularCPD(variable='Tanque', variable_card=2, values=[[0.05],[0.95]])
 
     #Chamando a função que aplica a técnica de redes Bayesianas
     cpdVazao = gerarResposta()
 
-    cpdSensor = TabularCPD('Sensor', 2, [0.95, 0.02], evidence='Tanque', evidence_card=[2])
+    cpdSensor = TabularCPD(variable='Sensor', variable_card=2, values=[[0.95, 0.02],[0.05, 0.98]], evidence=['Tanque'], evidence_card=[2])
 
     modelo.add_cpds(cpdPainel, cpdFiltro, cpdTanque, cpdVazao, cpdSensor)
 
@@ -59,4 +59,25 @@ def criarSistema():
 
 
 if __name__ == '__main__':
-    criarSistema()
+    modeloImplementado = criarSistema()
+
+    print(modeloImplementado)
+    inferencia = VariableElimination(modeloImplementado)
+
+    print("===== TESTE DE INFERENCIA (FILTRO) =====")
+    # Testando o impacto do filtro na falta de vazao
+    filtroResult = inferencia.query(variables=['Filtro'], evidence={'Vazao':0})
+
+    print(filtroResult)
+
+    print("===== TESTE DE INFERENCIA (PAINEL) =====")
+    # Testando o impacto do filtro na falta de vazao
+    painelResult = inferencia.query(variables=['Painel'], evidence={'Vazao':0})
+
+    print(painelResult)    
+
+    print("===== TESTE DE INFERENCIA (TANQUE) =====")
+    # Testando o impacto do filtro na falta de vazao
+    tanqueResult = inferencia.query(variables=['Tanque'], evidence={'Vazao':0})
+
+    print(tanqueResult)      
